8e5b5683e18f90eb9a223cbae623b474c9f25953
Liinna03 <liinna03@gmail.com>
2022-02-23 21:34:39 -0500

    fix() agregar la funcion is_present.py

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def before_scenario(context, scenario):
+def before_scenario(context, scenario, ):
     options = Options()
     options.add_argument("--no-sandbox")  # bypass OS security model
     options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
@@ -12,7 +12,9 @@ def before_scenario(context, scenario):
     options.add_argument("--disable-extensions")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
-    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(executable_path=r"C:\Users\amay801549\.wdm\drivers\chromedriver\win32\98.0.4758"
+                                                      r".102\chromedriver.exe")
     context.driver.maximize_window()
     context.driver.get("https://www.saucedemo.com/")
 
